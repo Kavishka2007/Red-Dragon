0bb96162cdb49674e3a26d97d88158e1fe28653e
Kavishka2007 <111114047+Kavishka2007@users.noreply.github.com>
2022-08-12 14:19:19 +0530

    Upchanges.js

--- settings.js
+++ settings.js
@@ -29,10 +29,10 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+94777729124'] // owner number
+global.ownername = "😎KAVISHKA❯❯" // owner name
 global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
+global.socialm ='Kavishka" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
@@ -44,19 +44,19 @@ global.antibad = "false" // To remove bad word senders
 global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "true" // bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94777729124']
+global.ownertag = '+94777729124' //your tag number
+global.botname = 'Kavishka- 𝘉𝘖𝘛' //ur bot name
+global.packname = 'Soical Media Club" // sticker packname
+global.author = "Kavishka" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[KAVISHKA ]' //
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰KAVISHKA☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://www.youtube.com/channel/UC_2oQqwrDZneocKOQJSrVJw' //Change Your Button's URL
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94777729124'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
